fb9863f4e92994bf8bb5bc375ddd3f3d8ada66fb
Kevin Ruiz <kleonardoruizflorez@gmail.com>
2025-02-25 18:35:49 +0100

    feat: should not show create task form fields if not clicked on create new task button

---
+++ src/sections/tasks/CreateTaskForm.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <form class="space-y-4 max-w-lg mx-auto p-6 bg-white rounded-lg shadow-md">
+    <div>
+      <label for="title" class="block text-sm font-medium text-gray-700">Title</label>
+      <input
+          type="text"
+          id="title"
+          name="title"
+          class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+      />
+    </div>
+
+    <div>
+      <label for="description" class="block text-sm font-medium text-gray-700">Description</label>
+      <textarea
+          id="description"
+          name="description"
+          rows="3"
+          class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+      ></textarea>
+    </div>
+
+    <div>
+      <label for="dueDate" class="block text-sm font-medium text-gray-700">Due Date</label>
+      <input
+          type="date"
+          id="dueDate"
+          name="dueDate"
+          class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+      />
+    </div>
+  </form>
+</template>
+
+<style scoped>
+
+</style>
--- src/sections/tasks/Tasks.vue
+++ src/sections/tasks/Tasks.vue
@@ -1,44 +1,22 @@
 <script setup lang="ts">
+import {ref} from "vue";
+import CreateTaskForm from "./CreateTaskForm.vue";
 
+const isShowingCreateTaskForm = ref(false);
+
+const showCreateTaskForm = () => {
+  isShowingCreateTaskForm.value = true;
+};
 </script>
 
 <template>
   <main class="flex flex-col gap-8">
     <button type="button"
+            @click="showCreateTaskForm"
             class="flex gap-2 items-center w-fit text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-base px-5 py-3">
       Create new task
     </button>
-    <form class="space-y-4 max-w-lg mx-auto p-6 bg-white rounded-lg shadow-md">
-      <div>
-        <label for="title" class="block text-sm font-medium text-gray-700">Title</label>
-        <input
-            type="text"
-            id="title"
-            name="title"
-            class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
-        />
-      </div>
-
-      <div>
-        <label for="description" class="block text-sm font-medium text-gray-700">Description</label>
-        <textarea
-            id="description"
-            name="description"
-            rows="3"
-            class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
-        ></textarea>
-      </div>
-
-      <div>
-        <label for="dueDate" class="block text-sm font-medium text-gray-700">Due Date</label>
-        <input
-            type="date"
-            id="dueDate"
-            name="dueDate"
-            class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
-        />
-      </div>
-    </form>
+    <CreateTaskForm v-if="isShowingCreateTaskForm"/>
   </main>
 </template>
 
