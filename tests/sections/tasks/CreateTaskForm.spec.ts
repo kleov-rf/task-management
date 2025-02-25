96ee721d602ea390ff7ef2b4bc8078c7f83d0973
Kevin Ruiz <kleonardoruizflorez@gmail.com>
2025-02-25 20:24:02 +0100

    test: not emit task created event is task creation fails

--- tests/sections/tasks/CreateTaskForm.spec.ts
+++ tests/sections/tasks/CreateTaskForm.spec.ts
@@ -67,4 +67,25 @@ describe('Create Task Form component', () => {
 
         expect(wrapper.emitted('task-created')).toBeTruthy();
     });
+    it('should not emit task created event is task creation fails', async () => {
+        const mockTaskCreator = {
+            create: vi.fn().mockRejectedValue()
+        }
+        const wrapper = mount(CreateTaskForm, {
+            global: {
+                provide: {
+                    taskCreator: mockTaskCreator
+                }
+            }
+        });
+
+        await wrapper.find('input[name="title"]').setValue('Awesome task');
+        await wrapper.find('textarea[name="description"]').setValue('Description of the awesome task');
+        await wrapper.find('input[name="dueDate"]').setValue('2021-12-31');
+
+        const confirmButton = wrapper.findAll('button').filter(b => b.text().match(/Confirm/))[0];
+        await confirmButton.trigger('click');
+
+        expect(wrapper.emitted('task-created')).toBeFalsy();
+    });
 });
