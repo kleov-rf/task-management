ea9e41f0a11aeb26ea3c8f760983b74d2efb51c1
Kevin Ruiz <kleonardoruizflorez@gmail.com>
2025-02-25 23:26:02 +0100

    feat(acceptance): delete a task

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,13 @@ import App from './App.vue'
 import {LocalStorageTaskRepository} from "./modules/tasks/infrastructure/LocalStorageTaskRepository.ts";
 import {TaskCreator} from "./modules/tasks/application/create/TaskCreator.ts";
 import {AllTasksGetter} from "./modules/tasks/application/get-all/AllTasksGetter.ts";
+import {TaskDeleter} from "./modules/tasks/application/delete/TaskDeleter.ts";
 
 const app = createApp(App);
 
 const taskRepository = new LocalStorageTaskRepository();
 app.provide('taskCreator', new TaskCreator(taskRepository));
 app.provide('allTasksGetter', new AllTasksGetter(taskRepository));
+app.provide('taskDeleter', new TaskDeleter(taskRepository));
 
 app.mount('#app')
