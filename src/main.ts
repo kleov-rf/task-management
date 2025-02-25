6911d3e65fc39e104f3398b6c6a88849aaaf7188
Kevin Ruiz <kleonardoruizflorez@gmail.com>
2025-02-25 20:55:59 +0100

    feat: provide all tasks getter in main

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import './style.css'
 import App from './App.vue'
 import {LocalStorageTaskRepository} from "./modules/tasks/infrastructure/LocalStorageTaskRepository.ts";
 import {TaskCreator} from "./modules/tasks/application/create/TaskCreator.ts";
+import {AllTasksGetter} from "./modules/tasks/application/get-all/AllTasksGetter.ts";
 
 const app = createApp(App);
 
 const taskRepository = new LocalStorageTaskRepository();
 app.provide('taskCreator', new TaskCreator(taskRepository));
+app.provide('allTasksGetter', new AllTasksGetter(taskRepository));
 
 app.mount('#app')
