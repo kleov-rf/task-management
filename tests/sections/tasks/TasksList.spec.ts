fc6c8821abd76a06d85440abd263593d3d2abf04
Kevin Ruiz <kleonardoruizflorez@gmail.com>
2025-02-25 22:34:18 +0100

    test: emit task deleted event when delete button is clicked

---
+++ tests/sections/tasks/TasksList.spec.ts
@@ -0,0 +1,26 @@
+import {describe, expect, it, vi} from "vitest";
+import {mount} from "@vue/test-utils";
+import TasksList from "../../../src/sections/tasks/TasksList.vue";
+import {Task} from "../../../src/modules/tasks/domain/Task";
+
+describe('Tasks list component', () => {
+    it('should emit task deleted event when delete button is clicked', async () => {
+        const mockTask = Task.create({
+            id: '1',
+            title: 'Task 1',
+            description: 'hello',
+            dueDate: new Date().getTime(),
+            status: 'pending'
+        });
+        const wrapper = mount(TasksList, {
+            props: {
+                tasks: [mockTask]
+            }
+        });
+
+        const deleteButton = wrapper.findAll('button').filter(b => b.text().match(/Delete/))[0];
+        await deleteButton.trigger('click');
+
+        expect(wrapper.emitted('task-deleted')).toBeTruthy();
+    })
+})
