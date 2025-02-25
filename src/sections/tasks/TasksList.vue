e40084e4bbc47432674cdfe5278581b16383e9c7
Kevin Ruiz <kleonardoruizflorez@gmail.com>
2025-02-25 22:46:29 +0100

    feat: call task deleter when delete button is clicked

--- src/sections/tasks/TasksList.vue
+++ src/sections/tasks/TasksList.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import type {Task} from "../../modules/tasks/domain/Task.ts";
 import TrashBinIcon from "./icons/TrashBinIcon.vue";
+import {inject} from "vue";
+import type {TaskDeleter} from "../../modules/tasks/application/delete/TaskDeleter.ts";
 
 defineProps<{
   tasks: Task[]
@@ -9,6 +11,13 @@ defineProps<{
 const emit = defineEmits<{
   (e: 'task-deleted'): void;
 }>()
+
+const taskDeleter = inject('taskDeleter') as TaskDeleter;
+
+const handleDeleteTask = async (id: string) => {
+    await taskDeleter.delete(id);
+    emit('task-deleted');
+}
 </script>
 
 <template>
@@ -55,7 +64,7 @@ const emit = defineEmits<{
         </td>
         <td class="flex justify-end px-6 py-4">
           <button type="button"
-                  @click="emit('task-deleted')"
+                  @click="handleDeleteTask(task.id)"
                   class="flex gap-2 items-center text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-3 py-2 text-center me-2 mb-2">
             <TrashBinIcon width="16" height="16"/>
             Delete
