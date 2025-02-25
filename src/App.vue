a0ebc38f5660dd68f996f2edacffbd6e9ebbb431
Kevin Ruiz <kleonardoruizflorez@gmail.com>
2025-02-25 18:18:35 +0100

    feat: show create task form fields when clicking on create new task button

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
+import Tasks from "./sections/tasks/Tasks.vue";
 </script>
 
 <template>
-  <h1 class="text-5xl font-extrabold">Task Manager</h1>
+  <section>
+    <h1 class="text-5xl font-extrabold">Task Manager</h1>
+    <Tasks/>
+  </section>
 </template>
 
 <style scoped>
