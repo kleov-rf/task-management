448d524c26b18e4bb5b38287feae07cec02f33c3
Kevin Ruiz <kleonardoruizflorez@gmail.com>
2025-02-25 23:00:41 +0100

    test: not call repository to delete a task if it does not exist

--- tests/modules/tasks/application/delete/TaskDeleter.spec.ts
+++ tests/modules/tasks/application/delete/TaskDeleter.spec.ts
@@ -29,4 +29,17 @@ describe('TaskDeleter', () => {
 
         expect(mockTaskRepository.get).toHaveBeenCalledWith('1');
     });
+    it('should not call repository to delete a task if it does not exist', async () => {
+        const mockTaskRepository = {
+            delete: vi.fn(),
+            getAll: vi.fn(),
+            save: vi.fn(),
+            get: vi.fn().mockResolvedValue(null)
+        } as TaskRepository
+        const taskDeleter = new TaskDeleter(mockTaskRepository);
+
+        await taskDeleter.delete('1');
+
+        expect(mockTaskRepository.delete).not.toHaveBeenCalled();
+    });
 })
