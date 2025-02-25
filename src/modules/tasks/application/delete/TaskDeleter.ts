273ffbc2d05c81c9818b87eeb5c90982aaa150cc
Kevin Ruiz <kleonardoruizflorez@gmail.com>
2025-02-25 22:57:48 +0100

    feat: call repository to check if task exists

--- src/modules/tasks/application/delete/TaskDeleter.ts
+++ src/modules/tasks/application/delete/TaskDeleter.ts
@@ -4,6 +4,7 @@ export class TaskDeleter {
     constructor(private readonly repository: TaskRepository) {}
 
     async delete(id: string): Promise<void> {
+        await this.repository.get(id);
         await this.repository.delete(id);
         return Promise.resolve();
     }
