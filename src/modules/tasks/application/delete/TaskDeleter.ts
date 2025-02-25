948eeaf3fece1aa02dae860d04e2bf12ea627892
Kevin Ruiz <kleonardoruizflorez@gmail.com>
2025-02-25 23:00:46 +0100

    feat: not call repository to delete a task if it does not exist

--- src/modules/tasks/application/delete/TaskDeleter.ts
+++ src/modules/tasks/application/delete/TaskDeleter.ts
@@ -1,11 +1,16 @@
 import type {TaskRepository} from "../../domain/TaskRepository.ts";
 
 export class TaskDeleter {
-    constructor(private readonly repository: TaskRepository) {}
+    constructor(private readonly repository: TaskRepository) {
+    }
 
     async delete(id: string): Promise<void> {
-        await this.repository.get(id);
-        await this.repository.delete(id);
+        const task = await this.repository.get(id);
+
+        if (task !== null) {
+            await this.repository.delete(id);
+        }
+
         return Promise.resolve();
     }
 }
