6bf4e726e4ee64ce1a468985bdb76b9c263620c2
Kevin Ruiz <kleonardoruizflorez@gmail.com>
2025-02-25 19:47:06 +0100

    feat: provide task creator

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import {LocalStorageTaskRepository} from "./modules/tasks/infrastructure/LocalStorageTaskRepository.ts";
+import {TaskCreator} from "./modules/tasks/application/create/TaskCreator.ts";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+const taskRepository = new LocalStorageTaskRepository();
+app.provide('taskCreator', new TaskCreator(taskRepository));
+
+app.mount('#app')
