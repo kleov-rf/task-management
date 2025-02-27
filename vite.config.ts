80ec195ab14b6c00a9535e0349d1fd8f960f4993
Kevin Ruiz <kleonardoruizflorez@gmail.com>
2025-02-27 20:48:56 +0100

    chore(deps): set up import alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
@@ -5,5 +6,10 @@ import eslintPlugin from '@nabla/vite-plugin-eslint'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss(), eslintPlugin()]
+  plugins: [vue(), tailwindcss(), eslintPlugin()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
