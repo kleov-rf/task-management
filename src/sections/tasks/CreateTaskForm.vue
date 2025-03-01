b21f6e7488a3e6c694081b764ae3eb1f33df601d
Kevin Ruiz <kleonardoruizflorez@gmail.com>
2025-03-01 21:59:45 +0100

    feat: call dialog close modal when cancel button is clicked

--- src/sections/tasks/CreateTaskForm.vue
+++ src/sections/tasks/CreateTaskForm.vue
@@ -29,6 +29,11 @@ const handleCreateTask = async () => {
   }
 }
 
+const handleCancelCreateTask = () => {
+  createTaskDialog.value?.close()
+  emit('cancel-create-task')
+}
+
 onMounted(() => {
   titleInput.value?.focus()
   createTaskDialog.value?.showModal()
@@ -102,7 +107,7 @@ onMounted(() => {
       <footer class="flex justify-end">
         <button
           type="button"
-          @click="emit('cancel-create-task')"
+          @click="handleCancelCreateTask"
           class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100"
         >
           Cancel
