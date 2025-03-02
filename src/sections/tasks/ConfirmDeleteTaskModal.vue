8438c63dddad5bd3902b28e8059bd5d9a1a4bf2c
Kevin Ruiz <kleonardoruizflorez@gmail.com>
2025-03-02 01:46:38 +0100

    style: darken cancel button focus ring

--- src/sections/tasks/ConfirmDeleteTaskModal.vue
+++ src/sections/tasks/ConfirmDeleteTaskModal.vue
@@ -43,7 +43,7 @@ onMounted(() => {
           ref="cancelDeletionButton"
           type="button"
           @click="handleCancelDeletion"
-          class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100"
+          class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-300"
         >
           Cancel
         </button>
